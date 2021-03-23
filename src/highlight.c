8c7b0dd3c65e531e77ee0631eb397fa7094f8c20
Unfit-Donkey <bjc2dogs@gmail.com>
2021-03-23 12:21:57 -0700

    Add color enum in highlight.c

--- src/highlight.c
+++ src/highlight.c
@@ -27,17 +27,39 @@
     19 - Control Flow (bold purple)
 */
 const char* syntaxTypes[] = { "Null","Numeral","Variable","Comment","Error","Bracket","Operator","String","Command","Space","Escape","Delimiter","Invalid Operator","Invalid Variable","Builtin","Custom","Argument","Unit","Local Variable","Control Flow" };
+typedef enum Color {
+    hl_null=0,
+    hl_number=1,
+    hl_variable=2,
+    hl_comment=3,
+    hl_error=4,
+    hl_bracket=5,
+    hl_operator=6,
+    hl_string=7,
+    hl_command=8,
+    hl_space=9,
+    hl_escape=10,
+    hl_delimiter=11,
+    hl_invalidOperator=12,
+    hl_invalidVariable=13,
+    hl_builtin=14,
+    hl_custom=15,
+    hl_argument=16,
+    hl_unit=17,
+    hl_localvar=18,
+    hl_controlFlow=19
+} Color;
 int getVariableType(const char* name, bool useUnits, char** argNames, char** localVars) {
     Tree tree = findFunction(name, useUnits, argNames, localVars);
-    if(tree.optype == -1) return 17;
+    if(tree.optype == -1) return hl_unit;
     if(tree.optype == optype_builtin) {
-        if(tree.op == 0) return 13;
-        return 14;
+        if(tree.op == 0) return hl_invalidVariable;
+        return hl_builtin;
     }
-    if(tree.optype == optype_custom) return 15;
-    if(tree.optype == optype_argument) return 16;
-    if(tree.optype == optype_localvar) return 18;
-    return 13;
+    if(tree.optype == optype_custom) return hl_custom;
+    if(tree.optype == optype_argument) return hl_argument;
+    if(tree.optype == optype_localvar) return hl_localvar;
+    return hl_invalidVariable;
 }
 void highlightSyntax(char* eq, char* out, char** args, char** localVars, int base, bool useUnits) {
     int eqLen = strlen(eq);
@@ -45,24 +67,24 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
     int end = start;
     while(eq[start] != 0) {
         while(eq[start] == ' ') {
-            out[start] = 9;
+            out[start] = hl_space;
             start++;
             if(eq[start] == 0) return;
         }
         if(eq[start] == ',' || eq[start] == ';') {
-            out[start] = 11;
+            out[start] = hl_delimiter;
             start++;
             continue;
         }
         int type = nextSection(eq, start, &end, base);
         int len = end - start + 1;
         if(type == -1) {
-            out[start] = 4;
+            out[start] = hl_error;
             start++;
             continue;
         }
         //Numbers
-        else if(type == 0) memset(out + start, 1, len);
+        else if(type == 0) memset(out + start, hl_number, len);
         //Variables
         else if(type == 1) {
             char name[len + 1];
@@ -97,11 +119,11 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
             memset(out + start, op.optype + 14, len);
             if(eq[end] == 0) {
                 start = firstParenth + 1;
-                out[firstParenth] = 4;
+                out[firstParenth] = hl_error;
                 continue;
             }
-            out[firstParenth] = 5;
-            out[end] = 5;
+            out[firstParenth] = hl_bracket;
+            out[end] = hl_bracket;
             char endOriginal = eq[end];
             eq[end] = 0;
             highlightSyntax(eq + firstParenth + 1, out + firstParenth + 1, args, localVars, base, useUnits);
@@ -110,13 +132,13 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
         //Parenthesis, square brackets or vectors
         else if(type == 4 || type == 3 || type == 7) {
             if(eq[end] == 0) {
-                out[start] = 4;
+                out[start] = hl_error;
                 start++;
                 if(type == 4) useUnits = true;
                 continue;
             }
-            out[start] = 5;
-            out[end] = 5;
+            out[start] = hl_bracket;
+            out[end] = hl_bracket;
             char endOriginal = eq[end];
             eq[end] = 0;
             highlightSyntax(eq + start + 1, out + start + 1, args, localVars, base, type == 4 ? true : useUnits);
@@ -128,12 +150,12 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
             //Find underscore
             int underscore = endBrac + 1;
             while(eq[underscore] != '_') {
-                out[underscore] = 9;
+                out[underscore] = hl_space;
                 underscore++;
             }
             //Color brackets
-            out[endBrac] = 5;
-            out[start] = 5;
+            out[endBrac] = hl_bracket;
+            out[start] = hl_bracket;
             //Calculate base
             eq[endBrac] = 0;
             double base = 0;
@@ -153,13 +175,13 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
                 freeValue(baseVal);
                 //Highlight as error if out of bounds
                 if(base < 1 || base > 36) {
-                    memset(out + underscore + 1, 4, end - underscore);
+                    memset(out + underscore + 1, hl_error, end - underscore);
                     base = 10;
                 }
                 else highlightSyntax(eq + underscore + 1, out + underscore + 1, NULL, NULL, base, false);
             }
             //Highlight underscore as operator
-            out[underscore] = 6;
+            out[underscore] = hl_operator;
             highlightSyntax(eq + start + 1, out + start + 1, args, localVars, base, true);
             eq[endBrac] = ']';
         }
@@ -192,14 +214,14 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
                 else isValid = false;
             }
             else isValid = false;
-            if(isValid) memset(out + start, 6, len);
-            else memset(out + start, 12, len);
+            if(isValid) memset(out + start, hl_operator, len);
+            else memset(out + start, hl_invalidOperator, len);
         }
         //Anonymous functions
         else if(type == 9 || type == 8) {
             int eqPos = highlightArgumentList(eq + start, out + start) + start;
-            out[eqPos] = 6;
-            out[eqPos + 1] = 6;
+            out[eqPos] = hl_operator;
+            out[eqPos + 1] = hl_operator;
             ignoreError = true;
             char** args = parseArgumentList(eq + start);
             ignoreError = false;
@@ -238,14 +260,14 @@ void highlightSyntax(char* eq, char* out, char** args, char** localVars, int bas
             else {
                 int startBracket = eqPos + 2;
                 while(eq[startBracket] != '{') startBracket++;
-                memset(out + eqPos + 2, 9, startBracket - eqPos - 2);
+                memset(out + eqPos + 2, hl_space, startBracket - eqPos - 2);
                 bool isEndBracket = eq[end] != 0;
                 if(isEndBracket) {
-                    out[startBracket] = 5;
-                    out[end] = 5;
+                    out[startBracket] = hl_bracket;
+                    out[end] = hl_bracket;
                     eq[end] = 0;
                 }
-                else out[startBracket] = 4;
+                else out[startBracket] = hl_error;
                 highlightCodeBlock(eq + startBracket + 1, out + startBracket + 1, args, NULL);
                 if(isEndBracket) eq[end] = '}';
             }
@@ -259,10 +281,10 @@ int highlightArgumentList(char* eq, char* out) {
     int i = 0;
     if(eq[0] == '(') {
         endPos = findNext(eq, 0, ')');
-        if(endPos == -1) out[0] = 4;
+        if(endPos == -1) out[0] = hl_error;
         else {
-            out[0] = 5;
-            out[endPos] = 5;
+            out[0] = hl_bracket;
+            out[endPos] = hl_bracket;
         }
         i++;
     }
@@ -271,23 +293,23 @@ int highlightArgumentList(char* eq, char* out) {
         if(eq[i] == '=' && i > endPos) return i;
         if(eq[i] == 0) return i;
         if(eq[i] == ' ') {
-            out[i] = 9;
+            out[i] = hl_space;
             continue;
         }
         if(eq[i] == ',') {
-            out[i] = 11;
+            out[i] = hl_delimiter;
             prevWasComma = true;
             continue;
         }
-        if(eq[i] >= 'A' && eq[i] <= 'Z') out[i] = 16;
-        else if(eq[i] >= 'a' && eq[i] <= 'z') out[i] = 16;
-        else if(eq[i] == '.' || eq[i] == '_') out[i] = 16;
-        else if(eq[i] >= '0' && eq[i] <= '9') out[i] = 16;
+        if(eq[i] >= 'A' && eq[i] <= 'Z') out[i] = hl_argument;
+        else if(eq[i] >= 'a' && eq[i] <= 'z') out[i] = hl_argument;
+        else if(eq[i] == '.' || eq[i] == '_') out[i] = hl_argument;
+        else if(eq[i] >= '0' && eq[i] <= '9') out[i] = hl_argument;
         if(prevWasComma) {
             prevWasComma = false;
-            if((eq[i] >= '0' && eq[i] <= '9') || eq[i] == '.') out[i] = 4;
+            if((eq[i] >= '0' && eq[i] <= '9') || eq[i] == '.') out[i] = hl_error;
         }
-        if(out[i] == 0) out[i] = 4;
+        if(out[i] == 0) out[i] = hl_error;
     }
 }
 char* highlightLine(char* eq) {
@@ -297,21 +319,21 @@ char* highlightLine(char* eq) {
     if((eq[0] == '/' && eq[1] == '/') || eq[0] == '#') {
         for(int i = 0;eq[i] != 0;i++) {
             //Set out to comment character
-            out[i] = 3;
+            out[i] = hl_comment;
             if(eq[i] == '$' && eq[i + 1] == '(') {
                 //Set $ to operator and find end bracket
-                out[i] = 6;
+                out[i] = hl_operator;
                 int endBrac = findNext(eq, i + 1, ')');
                 //If end bracket doesn't exist, set ( to error and highlight the rest
                 if(endBrac == -1) {
-                    out[i + 1] = 4;
+                    out[i + 1] = hl_error;
                     highlightSyntax(eq + i + 2, out + i + 2, NULL, globalLocalVariables, 10, false);
                     i = strlen(eq) - 1;
                 }
                 //Else set both to matching and set i to the end bracket
                 else {
-                    out[i + 1] = 5;
-                    out[endBrac] = 5;
+                    out[i + 1] = hl_bracket;
+                    out[endBrac] = hl_bracket;
                     eq[endBrac] = 0;
                     highlightSyntax(eq + i + 2, out + i + 2, NULL, globalLocalVariables, 10, false);
                     eq[endBrac] = ')';
@@ -324,8 +346,8 @@ char* highlightLine(char* eq) {
     if(eq[0] == '-') {
         //Find the position of the space
         while(eq[start] != ' ' && eq[start] != 0) start++;
-        memset(out, 8, start);
-        out[start] = 9;
+        memset(out, hl_command, start);
+        out[start] = hl_space;
         if(startsWith(eq, "-base") || startsWith(eq, "-unit")) {
             //Highlight the command suffix
             eq[start] = 0;
@@ -339,11 +361,11 @@ char* highlightLine(char* eq) {
             int firstParenth = start;
             while(eq[firstParenth] != '(' && eq[firstParenth] != 0) firstParenth++;
             //Highlight the name
-            memset(out + start + 1, 15, firstParenth - start - 1);
+            memset(out + start + 1, hl_custom, firstParenth - start - 1);
             //highlight the argument list and find the equal position
             int eqPos = highlightArgumentList(eq + firstParenth, out + firstParenth) + firstParenth;
             if(eq[eqPos] != 0) {
-                out[eqPos] = 8;
+                out[eqPos] = hl_command;
                 int next = eqPos + 1;
                 while(eq[next] == ' ') next++;
                 char** args = parseArgumentList(eq + firstParenth);
@@ -353,12 +375,12 @@ char* highlightLine(char* eq) {
                     int endBrac = findNext(eq, next, '}');
                     bool hasEndBracket = endBrac == -1;
                     if(hasEndBracket) {
-                        out[next] = 4;
+                        out[next] = hl_error;
                         endBrac = strlen(eq);
                     }
                     else {
-                        out[next] = 5;
-                        out[endBrac] = 5;
+                        out[next] = hl_bracket;
+                        out[endBrac] = hl_bracket;
                         eq[endBrac] = 0;
                     }
                     //Copy content and highlight
@@ -395,22 +417,22 @@ char* highlightLine(char* eq) {
         if(startsWith(eq, "-include ")) {
             for(int i = 0;i < includeFuncsLen;i++) {
                 if(strcmp(eq + 9, includeFuncs[i].name) == 0) {
-                    memset(out + 9, 15, strlen(eq + 9));
+                    memset(out + 9, hl_custom, strlen(eq + 9));
                     return out;
                 }
             }
-            memset(out + 9, 13, strlen(eq + 9));
+            memset(out + 9, hl_invalidVariable, strlen(eq + 9));
             return out;
         }
     }
     if(eq[0] == '.') {
-        out[0] = 8;
+        out[0] = hl_command;
         start++;
     }
     int isLocalVariable = isLocalVariableStatement(eq);
     if(isLocalVariable) {
-        memset(out, 2, isLocalVariable);
-        out[isLocalVariable] = 8;
+        memset(out, hl_localvar, isLocalVariable);
+        out[isLocalVariable] = hl_command;
         start = isLocalVariable + 1;
     }
     highlightSyntax(eq + start, out + start, NULL, globalLocalVariables, 10, false);
@@ -423,7 +445,7 @@ void highlightCodeBlock(char* eq, char* out, char** args, char** localVars) {
     int prevLineIsIf = false;
     int eqLen = strlen(eq);
     for(int i = 0;i < eqLen;i++) {
-        while(eq[i] == ' ') out[i++] = 9;
+        while(eq[i] == ' ') out[i++] = hl_space;
         //This loop parses each statement separated by ;
         char* type = eq + i;
         bool isCodeBlock = false;
@@ -434,43 +456,43 @@ void highlightCodeBlock(char* eq, char* out, char** args, char** localVars) {
         int isEqual = isLocalVariableStatement(eq + i);
         if(isEqual) {
             char* name = calloc(isEqual + 1, 1);
-            memset(out + i, 18, isEqual);
-            out[i + isEqual] = 8;
+            memset(out + i, hl_localvar, isEqual);
+            out[i + isEqual] = hl_command;
             memcpy(name, eq + i, isEqual);
             i = isEqual + 1;
             highlightSyntax(eq + i, out + i, args, localVarCopy, 10, false);
             argListAppend(&localVarCopy, name, &localVarSize);
         }
         else if(startsWith(type, "return")) {
-            memset(out + i, 19, 6);
+            memset(out + i, hl_controlFlow, 6);
             i += 6;
             if(i != end) highlightSyntax(eq + i, out + i, args, localVarCopy, 10, false);
         }
         else if(startsWith(type, "else")) {
-            if(prevLineIsIf) memset(out + i, 19, 4);
-            else memset(out + i, 4, 4);
+            if(prevLineIsIf) memset(out + i, hl_controlFlow, 4);
+            else memset(out + i, hl_error, 4);
             i += 4;
             isCodeBlock = true;
         }
         else if(startsWith(type, "if") || startsWith(type, "while")) {
             int typeLen = type[0] == 'i' ? 2 : 5;
-            memset(out + i, 19, typeLen);
+            memset(out + i, hl_controlFlow, typeLen);
             isCodeBlock = true;
             i += typeLen;
             int startBrac = i;
             while(eq[startBrac] != '(' && eq[startBrac] != 0) startBrac++;
             if(eq[startBrac] == 0) {
-                memset(out + i, 4, end - i);
+                memset(out + i, hl_error, end - i);
                 goto next;
             }
             int endBrac = findNext(eq, startBrac, ')');
             if(endBrac == -1) {
-                out[startBrac] = 4;
+                out[startBrac] = hl_error;
                 highlightSyntax(eq + startBrac + 1, out + startBrac + 1, args, localVarCopy, 10, false);
                 goto next;
             }
-            out[startBrac] = 5;
-            out[endBrac] = 5;
+            out[startBrac] = hl_bracket;
+            out[endBrac] = hl_bracket;
             eq[endBrac] = 0;
             highlightSyntax(eq + startBrac + 1, out + startBrac + 1, args, localVarCopy, 10, false);
             eq[endBrac] = ')';
@@ -480,22 +502,22 @@ void highlightCodeBlock(char* eq, char* out, char** args, char** localVars) {
         }
         else if(startsWith(type, "break") || startsWith(type, "continue")) {
             if(type[0] == 'b') {
-                memset(out + i, 19, 5);
+                memset(out + i, hl_controlFlow, 5);
                 i += 5;
             }
             else {
-                memset(out + i, 19, 8);
+                memset(out + i, hl_controlFlow, 8);
                 i += 8;
             }
-            memset(out + i, 4, end - i);
+            memset(out + i, hl_error, end - i);
         }
         else {
             //Set invalid characters to error
-            memset(out + i, 4, end - i);
+            memset(out + i, hl_error, end - i);
         }
         //Highlight rest of the statement
         if(isCodeBlock) {
-            while(eq[i] == ' ') out[i++] = 9;
+            while(eq[i] == ' ') out[i++] = hl_space;
             //Find open bracket
             int openBrac = eq[i] == '{' ? i : -1;
             if(openBrac == -1) {
@@ -507,13 +529,13 @@ void highlightCodeBlock(char* eq, char* out, char** args, char** localVars) {
             int closeBrac = findNext(eq, openBrac, '}');
             if(closeBrac == -1) {
                 //If doesn't exist, set open to error and highlight rest
-                out[openBrac] = 4;
+                out[openBrac] = hl_error;
                 highlightCodeBlock(eq + openBrac + 1, out + openBrac + 1, args, localVarCopy);
                 goto next;
             }
             //If they both exist, set both to valid brackets and highlight the innards
-            out[openBrac] = 5;
-            out[closeBrac] = 5;
+            out[openBrac] = hl_bracket;
+            out[closeBrac] = hl_bracket;
             eq[closeBrac] = 0;
             highlightCodeBlock(eq + openBrac + 1, out + openBrac + 1, args, localVarCopy);
             eq[closeBrac] = '}';
@@ -524,7 +546,7 @@ void highlightCodeBlock(char* eq, char* out, char** args, char** localVars) {
         if(endIsNull) break;
         else {
             eq[end] = ';';
-            out[end] = 11;
+            out[end] = hl_delimiter;
         }
     }
     freeArgList(localVarCopy);
